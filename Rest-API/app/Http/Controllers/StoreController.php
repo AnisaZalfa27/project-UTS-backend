4cddbe1bf47ce3c11b255709369798b9e7e37b81
Zalfa Khoirunnisa <145968770+AnisaZalfa27@users.noreply.github.com>
2024-11-13 16:08:11 +0700

    project backend

--- Rest-API/app/Http/Controllers/StoreController.php
+++ Rest-API/app/Http/Controllers/StoreController.php
@@ -6,17 +6,22 @@ use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function store (Request $request){
-       $validateData = $request-> validate([
+   public function store(Request $request) {
+
+        $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'content' => 'required|string',
-            
-       ]);
-
-            $new = New::create([
-
-            ]);
+        ]);
     
-
+        $news = news::create([ 
+            'title' => $validatedData['title'],
+            'content' => $validatedData['content'],
+        ]);
+    
+        return response()->json([
+            'message' => 'Resource added successfully',
+            'data' => $news,
+            'status_code' => 201
+        ], 201);
     }
 }
